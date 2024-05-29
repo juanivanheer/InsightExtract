acca8280b4b301c7cf66cb6c18ec352cde59887c
Juan Van Heerden <juanivanheer@gmail.com>
2024-05-30 07:51:28 +1000

    feat: first landing page iteration -> main title done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,7 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import Link from "next/link";
+import { ArrowRight } from "lucide-react";
+import { buttonVariants } from "@/components/ui/button";
 
 export default function Home() {
   return (
@@ -6,6 +9,24 @@ export default function Home() {
       <div className="mx-auto mb-4 flex max-w-fit items-center justify-center space-x-2 overflow-hidden rounded-full border border-gray-200 bg-white px-7 py-2 shadow-md backdrop-blur transition-all hover:border-gray-300 hover:bg-white/50">
         <p className="text-sm font-semibold text-gray-700">Published!</p>
       </div>
+      <h1 className="max-w-4xl text-5xl font-bold md:text-6xl lg:text-7xl">
+        Chat with your <span className="text-green-600">documents</span> in
+        seconds.
+      </h1>
+      <p className="mt-5 max-w-prose text-zinc-700 sm:text-lg">
+        InsightExtract allows you to have conversations with ay PDF document.
+        Simply upload your file and start asking questions right away
+      </p>
+      <Link
+        className={buttonVariants({
+          size: "lg",
+          className: "mt-5",
+        })}
+        href="/dashboard"
+        target="_blank"
+      >
+        Get started <ArrowRight className="ml-2 h-5 w-5" />
+      </Link>
     </MaxWidthWrapper>
   );
 }
