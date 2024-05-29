0699508cbc24acc3915047422a36486d4015642b
Juan Van Heerden <juanivanheer@gmail.com>
2024-05-29 11:03:17 +1000

    feat: created MaxWidthWrapper component as the whole app wrapper component

--- src/components/MaxWidthWrapper.tsx
+++ src/components/MaxWidthWrapper.tsx
@@ -1,3 +1,4 @@
+import { cn } from "@/lib/utils";
 import { ReactNode } from "react";
 
 type MaxWidthWrapperType = {
@@ -7,7 +8,12 @@ type MaxWidthWrapperType = {
 
 const MaxWidthWrapper = ({ className, children }: MaxWidthWrapperType) => {
   return (
-    <div className="mx-auto w-full max-w-screen-xl px-2.5 md:px-20">
+    <div
+      className={cn(
+        "mx-auto w-full max-w-screen-xl px-2.5 md:px-20",
+        className
+      )}
+    >
       {children}
     </div>
   );
