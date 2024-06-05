6792e3daee770a989fc208972506ed25b8124902
Juan Van Heerden <juanivanheer@gmail.com>
2024-06-05 11:27:22 +1000

    feat: created new components for the chat section

--- src/app/dashboard/[fileId]/page.tsx
+++ src/app/dashboard/[fileId]/page.tsx
@@ -1,4 +1,4 @@
-import ChatWrapper from "@/components/ChatWrapper";
+import ChatWrapper from "@/components/chat/ChatWrapper";
 import PdfRenderer from "@/components/pdf-reader/PdfRenderer";
 import { db } from "@/db";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
---
+++ src/components/chat/ChatInput.tsx
@@ -0,0 +1,3 @@
+const ChatInput = () => <div></div>;
+
+export default ChatInput;
