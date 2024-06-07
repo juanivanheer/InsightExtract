4dc92e5b2416517adb9364d80d962417945d4bf4
Juan Van Heerden <juanivanheer@gmail.com>
2024-06-07 15:42:08 +1000

    feat: changed chatInput form for a div to avoid weird behaviours

--- src/components/chat/ChatInput.tsx
+++ src/components/chat/ChatInput.tsx
@@ -31,7 +31,7 @@ const ChatInput = ({ isDisabled }: ChatInputProps) => {
 
   return (
     <div className="absolute bottom-0 left-0 w-full">
-      <form className="mx-2 flex flex-row gap-3 md:mx-4 md:last:mb-6 lg:mx-auto lg:max-w-2xl xl:max-w-3xl">
+      <div className="mx-2 flex flex-row gap-3 md:mx-4 md:last:mb-6 lg:mx-auto lg:max-w-2xl xl:max-w-3xl">
         <div className="relative flex h-full flex-1 items-stretch md:flex-col">
           <div className="relative flex flex-col w-full flex-grow p-4">
             <div className="relative">
@@ -51,7 +51,6 @@ const ChatInput = ({ isDisabled }: ChatInputProps) => {
                 aria-label="send message"
                 className="absolute bottom-1.5 right-[8px]"
                 disabled={isLoading || isDisabled}
-                type="submit"
                 onClick={onButtonClick}
               >
                 <Send className="h-4 w-4" />
@@ -59,7 +58,7 @@ const ChatInput = ({ isDisabled }: ChatInputProps) => {
             </div>
           </div>
         </div>
-      </form>
+      </div>
     </div>
   );
 };
